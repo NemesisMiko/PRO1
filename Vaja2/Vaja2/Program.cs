8f974182a2618bb6e0f7d510f29b84a0303d4be7
Alen <kogojmiha@gmail.com>
2023-01-20 09:47:03 +0100

    vaja2

---
+++ Vaja2/Vaja2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaja2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stari Donald farmo ima");
+            Console.WriteLine("IAIAIO");
+            Console.WriteLine("Na tej farmi osla dva ");
+            Console.WriteLine("IAIAO");
+        }
+    }
+}
